690c3669c080c86f9806399887178f0b1bae06d4
DanielHolo <idaniel.com1@gmail.com>
2020-07-06 20:06:59 +0300

    project start

---
+++ src/currency_exchange/settings_local.dev.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'm)mrte0xqk3x$qsg7s4=g)&*pxoo50p9#5=zl#c#ume+4$%k_g'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
